9988dfffbffa22ab2658cd368548c65a33aaa8f7
tuongnymo <tuongnymo@gmail.com>
2025-05-11 17:35:03 +0700

    C?p nh?t hoàn ch?nh giao di?n và s?n ph?m

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(window.location.search);
-const type = params.get("type");
+const category = params.get("category");
 
 const typeMap = {
   "giay-nam": "Giày nam",
@@ -8,15 +8,4 @@ const typeMap = {
   "phu-kien": "Phụ kiện"
 };
 
-document.getElementById("product-title").innerText = typeMap[type] || "Sản phẩm";
-// Lấy tham số từ URL
-const params = new URLSearchParams(window.location.search);
-const category = params.get("category");
-
-// Load và lọc dữ liệu
-fetch('example.json')
-  .then(res => res.json())
-  .then(data => {
-    const filtered = data.filter(p => p.category === category);
-    renderProducts(filtered); // Hàm hiển thị sản phẩm
-});
+document.getElementById("product-title").innerText = typeMap[category] || "Sản phẩm";
