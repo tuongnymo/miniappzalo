e22c1de34d192c19232f3b8a037499a5dd83ec38
tuongnymo <tuongnymo@gmail.com>
2025-05-12 15:18:53 +0700

    C?p nh?t hoàn ch?nh giao di?n và s?n ph?m

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (!window.CATEGORY) return;
 
-  fetch('example.json')
+  fetch('https://miniappzalo.vercel.app/example.json')
     .then(res => res.json())
     .then(data => {
       let filtered = data.filter(p => p.category === CATEGORY);
